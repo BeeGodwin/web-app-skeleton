4d8d7342d1512659145268c1afe9f00991efc10c
Bee Godwin <bee.godwin@bbc.co.uk>
2020-05-29 17:57:39 +0100

    unit test command, add sinon

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,9 @@
-
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 const { setupDOM } = require('./setup/dom');
 Enzyme.configure({adapter: new Adapter()});
 setupDOM(global);
+
+// arguably, import expect, mount, render, shallow and make globally available. once imported-
+// global.expect = expect;
--- test/unit/app/App-test.jsx
+++ test/unit/app/App-test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { expect } from 'chai';
-import App from '../../src/app/App';
+import sinon from 'sinon';
+import App from '../../../src/app/App';
+
+const sandbox = sinon.createSandbox();
 
 describe('App component', () => {
+
+  afterEach(() => sandbox.restore());
+
   it('renders an h1', () => {
     const wrapper = mount(<App />);
     const node = wrapper.find('h1');
