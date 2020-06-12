ebfa91ad9a9eb2d8d50a66acebe452a80257d19f
Bee Godwin <bee.godwin@bbc.co.uk>
2020-06-12 15:12:34 +0100

    hide client

--- src/app/clients/rms.js
+++ src/app/clients/rms.js
@@ -1,9 +1,8 @@
 import RMSClient from '@bbc/rms-client';
 
-export const client = RMSClient.createClient({
+const client = RMSClient.createClient({
   apiKey: process.env.MS_CLIENT_API_KEY
 });
-
 
 export const getProgramme = (pid) => {
   const query = client.v2ProgrammePlayable().pid(pid);
