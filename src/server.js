da7495701a478a8bb25782c9d019ad4252d152a3
Bee Godwin <bee.godwin@bbc.co.uk>
2020-06-12 12:22:18 +0100

    successful get from rms-client. happydance

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const RMSClient = require('@bbc/rms-client');
 
 const app = express();
 const port = 3000;
 
-app.get('/path', (req, res) => res.send('This is a route'));
+const rmsClient = RMSClient.createClient({
+  apiKey: process.env.RMS_CLIENT_API_KEY
+});
+
+app.get('/rms', (req, res) => {
+  const query = rmsClient.v2ProgrammePlayable().pid('p088s3d2');
+
+  rmsClient.get(query)
+  .then(
+    ({body, headers, statusCode}) => {res.send(body);},
+    err => { res.send(err);});
+  // res.send('This is a route');
+});
 
 app.use(express.static('dist'));
 
