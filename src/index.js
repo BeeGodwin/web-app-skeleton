b10c31a7d0a6b27f2bc78402bdfb5ebe3137202c
Bee Godwin <bee.godwin@bbc.co.uk>
2020-06-12 10:26:25 +0100

    added express but fails

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/app/App.jsx';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from '../src/app/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
