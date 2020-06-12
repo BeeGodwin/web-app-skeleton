ffca7b333c4b902c8f25887f01625a9bcf672031
Bee Godwin <bee.godwin@bbc.co.uk>
2020-06-12 13:05:30 +0100

    app renders a prop

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App data={'have some data'}/>, document.getElementById('root'));
