e5ad562e5e53470d667a75ead21fc94501d0f56c
Bee Godwin <bee.godwin@bbc.co.uk>
2020-06-12 11:42:26 +0100

    express server running serving static React content

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from '../src/app/App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/App.jsx';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-const express = require('express');
-
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+ReactDOM.render(<App />, document.getElementById('root'));
