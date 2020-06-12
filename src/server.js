75e9b52925cdcfa8985451f46488a1ff82d50995
Bee Godwin <bee.godwin@bbc.co.uk>
2020-06-12 11:56:43 +0100

    Example route

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// app.get('/', (req, res) => res.send('Hello World'));
+app.get('/path', (req, res) => res.send('This is a route'));
+
 app.use(express.static('dist'));
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
