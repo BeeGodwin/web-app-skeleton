c66d36ab6666dc25d61cf9b58ef881cbbbe72243
Bee Godwin <bee.godwin@bbc.co.uk>
2020-06-26 16:16:11 +0100

    update mini sample app

--- src/app/containers/BrandViewContainer.jsx
+++ src/app/containers/BrandViewContainer.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import get from 'lodash/get';
 
-const BrandViewContainer = () => {
+const BrandViewContainer = (props) => {
+  
+  // eslint-disable-next-line react/prop-types
+  const { title, synopsis } = props;
+
   return (
     <React.Fragment>
       <h1>Brand View</h1> 
+      <h2>{title}</h2>
+      <div>{synopsis}</div>
     </React.Fragment>
   );
 };
 
-export default BrandViewContainer;
+const mapStateToProps = (state) => {
+  return {
+    title: get(state, 'container.title'),
+    synopsis: get(state, 'container.synopses.short')
+  };
+};
+
+
+export default connect(mapStateToProps)(BrandViewContainer);
--- src/app/containers/EpisodeViewContainer.jsx
+++ src/app/containers/EpisodeViewContainer.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import get from 'lodash/get';
 
-const EpisodeViewContainer = () => {
+const EpisodeViewContainer = (props) => {
+
+  // eslint-disable-next-line react/prop-types
+  const {primaryTitle, secondaryTitle, tertiaryTitle, synopsis } = props;
   return (
     <React.Fragment>
       <h1>Episode View</h1> 
+      <h2>{primaryTitle} - {secondaryTitle} - {tertiaryTitle}</h2>
+      <div>{synopsis}</div>
     </React.Fragment>
   );
 };
 
-export default EpisodeViewContainer;
+const mapStateToProps = (state) => {
+  return {
+    primaryTitle: get(state, 'titles.primary'),
+    secondaryTitle: get(state, 'titles.secondary'),
+    tertiaryTitle: get(state, 'titles.tertiary'),
+    synopsis: get(state, 'synopses.short'),
+  };
+};
+
+export default connect(mapStateToProps)(EpisodeViewContainer);
